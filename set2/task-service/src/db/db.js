5b28cf16af7a86da3d03ca2e11f88173ab7793f3
PhacharaChangyouang <potbunpal@gmail.com>
2026-03-17 20:22:36 +0700

    fix: add tasakllvsvdvdkoavpakl

--- set2/task-service/src/db/db.js
+++ set2/task-service/src/db/db.js
@@ -1,9 +1,10 @@
 const { Pool } = require('pg');
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
-screen
+
 const initDB = async () => {
   try {
     await pool.query(`
